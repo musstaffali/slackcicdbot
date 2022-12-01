f867d46644ad252e78fec245fef5e79ada15dd83
musstaffali <mamustafa@gtcc.edu>
2022-12-01 13:28:42 -0500

    file twin

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
 const slackToken = process.env.SLACK_TOKEN;
 const port = process.env.SLACK_PORT || 3000
 
-
 const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken)
 
@@ -19,3 +18,9 @@ slackEvents.on('app_mention', (event) => {
       }
     })();
   });
+
+  slackEvents.on('error', console.error);
+
+slackEvents.start(port).then(() => {
+  console.log(`Server started on port ${port}`)
+});
