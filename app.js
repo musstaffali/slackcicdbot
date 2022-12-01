66fe9187dba4404d5897f1c266b9082d5f29f731
musstaffali <mamustafa@gtcc.edu>
2022-12-01 13:04:12 -0500

    latest unknown push

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { WebClient } = require('@slack/web-api')
 const { createEventAdapter } = require('@slack/events-api')
 
-const slackSigningSecret = ""
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
+const slackToken = process.env.SLACK_TOKEN;
 
 const slackEvents = createEventAdapter(slackSigningSecret);
+const slackClient = new WebClient(slackToken)
