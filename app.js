554db657a8291308bd94322df7bd8b49b2172894
musstaffali <mamustafa@gtcc.edu>
2022-12-01 13:15:38 -0500

    port & slack event creater

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const { createEventAdapter } = require('@slack/events-api')
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
 const slackToken = process.env.SLACK_TOKEN;
+const port = process.env.SLACK_PORT || 3000
+
 
 const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken)
+
+slackEvents = createEventAdapter(slackSigningSecret)
